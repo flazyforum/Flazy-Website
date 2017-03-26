ed9f9fdb06da351430d9cf0dc5481b2b58871b84
Gabriel Kanev <contact@mrgkanev.eu>
2017-03-26 12:18:55 +0300

    Just the first upload
    Just the first upload

---
+++ about.php
@@ -0,0 +1,71 @@
+<? include"header.php"; ?>
+<body>
+  <nav class="flazy-color" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="#" class="brand-logo">&lt;&#47;flazy&gt;</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="/downloads">Downloads</a></li>
+        <li><a href="/news">News</a></li>
+        <li><a href="/docs">Documentations</a></li>
+        <li><a href="/forum">Community</a></li>
+        <li class="active"><a href="/about" class="active">About</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="/downloads">Downloads</a></li>
+        <li><a href="/news">News</a></li>
+        <li><a href="/docs">Documentations</a></li>
+        <li><a href="/forum">Community</a></li>
+        <li class="active"><a href="/about" class="active">About</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+
+
+<main>
+  <div class="container">
+    <div class="section">
+      <div class="row">
+        <div class="col s12 m10">
+          <h3 class="header">Who?What?Where? - Flazy</h3>
+          <div class="card horizontal">
+            <div class="card-image">
+              <img src="https://scontent-frt3-1.xx.fbcdn.net/t31.0-8/13996190_906812842795790_6846193104689302830_o.png">
+            </div>
+            <div class="card-stacked">
+              <div class="card-content">
+                <p>Here we will give all kind of information about Flazy in general.</p>
+              </div>
+              <div class="card-action">
+                <a href="#">This is a link</a>
+              </div>
+            </div>
+          </div>
+        </div>
+		        <div class="col s12 m2 ">
+          <div class="card white">
+            <div class="card-content">
+              <span class="card-title">Advertisments</span>
+                <p>
+					<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+					<!-- Flazy.org -->
+					<ins class="adsbygoogle"
+						 style="display:block"
+						 data-ad-client="ca-pub-1909099540823981"
+						 data-ad-slot="3647691154"
+						 data-ad-format="auto"></ins>
+					<script>
+					(adsbygoogle = window.adsbygoogle || []).push({});
+					</script>
+				</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+  <? include "footer.php"; ?>
---
+++ index.php
@@ -0,0 +1,92 @@
+<? include"header.php"; ?>
+<body>
+  <nav class="flazy-color" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="#" class="brand-logo">	&lt;&#47;flazy&gt;</a>
+      <ul class="right hide-on-med-and-down">
+        <li class="active"><a href="index.php" class="active">Home</a></li>
+        <li><a href="/downloads">Downloads</a></li>
+        <li><a href="/news">News</a></li>
+        <li><a href="/docs">Documentations</a></li>
+        <li><a href="/forum">Community</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="side-nav">
+          <li class="active"><a href="index.php" class="active">Home</a></li>
+        <li><a href="/downloads">Downloads</a></li>
+        <li><a href="/blog">Blog</a></li>
+        <li><a href="/docs">Documentations</a></li>
+        <li><a href="/forum">Community</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+
+  <div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <br><br>
+        <h1 class="header center flazy-text text-lighten-2" style="text-shadow: 2px -4px 16px rgb(255, 255, 255);">Make simple forum with Flazy</h1>
+        <div class="row center">
+            <h5 class="header col s12 light" style="text-shadow: 0px 0px 2px rgb(0, 0, 0);">Light, fast and easy forum system based on Material Design</h5>
+        </div>
+        <div class="row center">
+          <a href="https://github.com/flazy-forum/Flazy-Forum/archive/0.7.1.zip" id="download-button" class="btn-large waves-effect waves-light flazy-color lighten-1">Download</a>
+        </div>
+        <br><br>
+
+      </div>
+    </div>
+    <div class="parallax"><img src="assets/background1.jpg" alt="Unsplashed background img 1"></div>
+  </div>
+
+
+  <div class="container">
+    <div class="section">
+
+      <!--   Icon Section   -->
+      <div class="row">
+        <div class="col s12 m3">
+          <div class="icon-block">
+            <h2 class="center flazy-text"><i class="material-icons">brush</i></h2>
+            <h5 class="center">Elegant Design</h5>
+
+            <p class="light" style="text-align: justify;">Flazy looks awesome through any device. To reach this goal we decided to use Google Material Design.</p>
+          </div>
+        </div>
+
+        <div class="col s12 m3">
+          <div class="icon-block">
+            <h2 class="center flazy-text"><i class="material-icons">business</i></h2>
+            <h5 class="center">Search Engine Optimization</h5>
+
+            <p class="light" style="text-align: justify;"></p>
+          </div>
+        </div>
+
+        <div class="col s12 m3">
+          <div class="icon-block">
+            <h2 class="center flazy-text"><i class="material-icons">settings</i></h2>
+            <h5 class="center">Fast, Lightweight with customs permissions</h5>
+
+            <p class="light" style="text-align: justify;">Flazy is lightning fast. The front-end is fully powered by JavaScript. We've made sure Flazy's assets are as small as possible, so your users won't run away because a page is taking too long to load. Take control of your forum with a single click.
+Assign permissions to the group for additional flexibility.</p>
+          </div>
+        </div>
+
+        <div class="col s12 m3">
+          <div class="icon-block">
+            <h2 class="center flazy-text"><i class="material-icons">build</i></h2>
+            <h5 class="center">Easy to work with</h5>
+
+            <p class="light" style="text-align: justify;">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Flazy.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <? include "footer.php"; ?>
